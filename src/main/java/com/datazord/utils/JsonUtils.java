e797431cd9623926a025198213fb2f80b1d710de
profmoh <prof.mohamedmagdy@gmail.com>
2018-06-28 23:45:05 +0300

    By Mohamed

--- src/main/java/com/datazord/utils/JsonUtils.java
+++ src/main/java/com/datazord/utils/JsonUtils.java
@@ -1,15 +1,107 @@
 package com.datazord.utils;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.xml.sax.SAXException;
+
 import com.datazord.json.tomato.pojo.product.Product;
+import com.datazord.json.tomato.pojo.product.productCustomOption;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 
 public class JsonUtils {
 
+//	public static void main(String[] args) {
+//		Product product = new Product();
+//
+//		productCustomOption p = new productCustomOption();
+//
+//		p.setImage("image");
+//		product.setProduct_custom_option(Arrays.asList(p));
+//
+//		setObjectByValueAndPath(product, "product.product_custom_option.image", "sdd");
+//		
+//		
+//		
+//		
+//		List<JsonObject> jsonObjectList = null;
+//
+//		try {
+//			jsonObjectList = FileUtils.readJsonObjectsFormXML("C:\\Users\\Mohamed\\Desktop\\Items.xml");
+//		} catch (SAXException | IOException | ParserConfigurationException | XPathExpressionException e) {
+//			e.printStackTrace();
+//			return;
+//		}
+//
+//		for (JsonObject jsonObject : jsonObjectList) {
+//			System.out.println("\n\n" + new GsonBuilder().setPrettyPrinting().create().toJson(jsonObject));
+//			System.out.println("\n\n\n");
+//			System.out.println(getStringValueFromJsonByPath(jsonObject, "#document :: DataTable :: diffgr:diffgram :: DocumentElement :: Items :: group_name"));
+//			break;
+//		}
+//	}
+
 	public static String getStringValueFromJsonByPath(JsonObject jsonObject, String path) {
-		return null;
+		if(StringUtils.isAllBlank(path) || jsonObject == null)
+			return null;
+
+		String[] jpathArray = path.split(" :: ");
+		String propertyName = jpathArray[jpathArray.length - 1];
+
+		JsonObject currentObject = jsonObject;
+
+		for (int i = 0; i < jpathArray.length - 1; i++)
+			try {
+				currentObject = currentObject.getAsJsonObject(jpathArray[i]);
+			} catch (Exception e) {
+				System.out.println(path + "   ^^^^^^^^^^^   " + jpathArray[i]);
+				return null;
+			}
+
+		return currentObject.get(propertyName).getAsString(); //.addProperty(propertyName, textContent)
 	}
 
-	public static void setObjectByValueAndPath(Product product, String path, String value) {
-		
+	public static void setObjectByValueAndPath(Product product, String path, Object value) {
+		if(StringUtils.isAllBlank(path) || value == null || product == null)
+			return;
+
+		String[] pathArray = path.split("\\.");
+
+		Object currentClass = product;
+		Class<?> objClass = currentClass.getClass();
+
+		for(int i = 1; i < pathArray.length; i++) {
+			try {
+				Field field = objClass.getDeclaredField(pathArray[i]);
+
+				if(i == pathArray.length - 1) {
+					field.set(currentClass, value);
+					break;
+				} else
+					currentClass = field.get(currentClass);
+
+				objClass = currentClass.getClass();
+
+				if(currentClass instanceof Collection<?>) {
+					Object[] objectArray = ((Collection<?>)currentClass).toArray();
+					currentClass = objectArray[objectArray.length - 1];
+					objClass = currentClass.getClass();
+				}
+			} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+				e.printStackTrace();
+				return;
+			}
+		}
 	}
 }
