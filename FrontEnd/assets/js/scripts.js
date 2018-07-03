c849589b4b4852d15d650ba8a5fdc72305984e90
khaled <khaled.elamir-ibrahim@vodafone.com>
2018-07-03 14:47:31 +0200

    add new Fron End

--- FrontEnd/assets/js/scripts.js
+++ FrontEnd/assets/js/scripts.js
@@ -5,27 +5,39 @@ mapping.config(function ($stateProvider, $urlRouterProvider, $qProvider) {
    
     $qProvider.errorOnUnhandledRejections(false);
 
-    $urlRouterProvider.otherwise('/mapping');
+    $urlRouterProvider.otherwise('/services');
     $urlRouterProvider.when('', '/login');
     $stateProvider.state('login', {
         url: '/login',
         templateUrl: 'views/login.html',
 
     })
-    $stateProvider.state('mapping', {
+    $stateProvider.state('services', {
+        url: '/services',
+        templateUrl: 'views/services.html',
+
+    }).state('services.configuration', {
+        url: '/configuration',
+        templateUrl: 'views/configuration.html',
+
+    }).state('services.addProduct', {
+        url: '/add-product',
+        templateUrl: 'views/addProduct.html',
+
+    }).state('services.mapping', {
         url: '/mapping',
         templateUrl: 'views/mapping.html',
 
-    }).state('mapping.category', {
+    }).state('services.mapping.category', {
         url: '/category',
         templateUrl: 'views/category.html',
-    }).state('mapping.color', {
+    }).state('services.mapping.color', {
         url: '/color',
         templateUrl: 'views/color.html',
-    }).state('mapping.size', {
+    }).state('services.mapping.size', {
         url: '/size',
         templateUrl: 'views/size.html',
-    }).state('mapping.product', {
+    }).state('services.mapping.product', {
         url: '/product',
         templateUrl: 'views/product.html',
     })
